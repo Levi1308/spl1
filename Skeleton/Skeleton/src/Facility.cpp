19c16f59a18eb94e235e87c249a2f6f64849ad13
Levi1308 <akivalev@post.bgu.ac.il>
2024-11-27 19:45:53 +0000

    Facility

--- Skeleton/Skeleton/src/Facility.cpp
+++ Skeleton/Skeleton/src/Facility.cpp
@@ -1,10 +1,32 @@
 #include "Facility.h"
+#include "SelectionPolicy.h"
 #include "iostream"
 #include <string>
 #include <vector>
 using std::string;
 using std::vector;
 using namespace std;
+
+// Constructor of FacilityType
+FacilityType::FacilityType(const string &FTname, const FacilityCategory FTcategory, const int FTprice, const int FTlifeQuality_score, const int FTeconomy_score, const int FTenvironment_score)
+    : name(FTname), category(FTcategory), price(FTprice), lifeQuality_score(FTlifeQuality_score), economy_score(FTeconomy_score), environment_score(FTenvironment_score) {}
+
+// Copy Constructor for FacilityType
+FacilityType::FacilityType(const FacilityType &other)
+    : name(other.name), category(other.category), price(other.price),
+      lifeQuality_score(other.lifeQuality_score), economy_score(other.economy_score), environment_score(other.environment_score) {}
+
+
+// Getter methods for FacilityType
+const string &FacilityType::getName() const { return name; }
+int FacilityType::getCost() const { return price; }
+int FacilityType::getLifeQualityScore() const { return lifeQuality_score; }
+int FacilityType::getEnvironmentScore() const { return environment_score; }
+int FacilityType::getEconomyScore() const { return economy_score; }
+FacilityCategory FacilityType::getCategory() const { return category; }
+
+
+
 
 // Constructor of Facility (inherits from FacilityType)
 Facility::Facility(const string &Fname, const string &FsettlementName, const FacilityCategory Fcategory, const int Fprice, const int FlifeQuality_score, const int Feconomy_score, const int Fenvironment_score)
